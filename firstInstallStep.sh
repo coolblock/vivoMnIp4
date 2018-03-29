63a045573e20c9f654679c40a997dc35412d8cbd
coolblock <34201898+coolblock@users.noreply.github.com>
2018-03-28 22:46:50 -0700

    cleaned it up

--- firstInstallStep.sh
+++ firstInstallStep.sh
@@ -1,4 +1,6 @@
-#install proper conf file on root 
+##!/usr/bin/env bash
+#set -x
+#trap read debug
 cd
 screen -v > /root/scrt.txt
 mkdir /var/log/sentinel
@@ -10,7 +12,6 @@ mv install.sh install.bak
 mv outputx install.sh
 chmod +x install.sh
 ./install.sh -p vivo -n 4 
-/usr/local/bin/activate_masternodes_vivo
 rm -f /etc/masternodes/vivo_n1.conf
 /usr/local/bin/activate_masternodes_vivo
 #cp /root/ /etc/masternodes/vivo_n1.conf
@@ -18,7 +19,6 @@ rm -f /etc/masternodes/vivo_n1.conf
 chown -R masternode:masternode /root/finalVivo.conf
 cp /root/finalVivo.conf /etc/masternodes/vivo_n1.conf  
 mkdir /home/masternode/.vivocore/
-
 cd /home/masternode
 sudo apt-get update
 sudo apt-get install -y git python-virtualenv
