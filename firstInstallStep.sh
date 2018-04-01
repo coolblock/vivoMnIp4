037ccbfcf3e3eca1e84d3e1a20e0b31e3686e772
coolblock <34201898+coolblock@users.noreply.github.com>
2018-03-31 17:15:36 -0700

    runmultiple in here

--- firstInstallStep.sh
+++ firstInstallStep.sh
@@ -17,23 +17,24 @@ rm -f /etc/masternodes/vivo_n1.conf
 #cp /root/ /etc/masternodes/vivo_n1.conf
 #copy proper conf file
 chown -R masternode:masternode /root/finalVivo.conf
+### next is final vivo
 cp /root/finalVivo.conf /etc/masternodes/vivo_n1.conf  
-mkdir /home/masternode/.vivocore/
+#mkdir /home/masternode/.vivocore/
 cd /home/masternode
-sudo apt-get update
-sudo apt-get install -y git python-virtualenv
-mkdir sentineldir
-cd sentineldir
-git clone https://github.com/coolblock/vivo_sentinel.git
-mv ./vivo_sentinel ./sentinel
-cd sentinel
-sudo apt-get install -y virtualenv
-virtualenv venv
-venv/bin/pip install -r requirements.txt
-rm sentinel.conf
-cp /root/sentinel.conf sentinel.conf
+#sudo apt-get update
+#sudo apt-get install -y git python-virtualenv
+#mkdir sentineldir
+#cd sentineldir
+#git clone https://github.com/coolblock/vivo_sentinel.git
+#mv ./vivo_sentinel ./sentinel
+#cd sentinel
+#sudo apt-get install -y virtualenv
+#virtualenv venv
+#venv/bin/pip install -r requirements.txt
+#rm sentinel.conf
+#cp /root/sentinel.conf sentinel.conf
 chown -R masternode:masternode /home/masternode/
-(crontab -l 2>/dev/null; echo "* * * * * /root/runsentinel.sh") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * /root/runmultipleSentinel.sh") | crontab -
 crontab -l
 
 touch /root/installCompleted
