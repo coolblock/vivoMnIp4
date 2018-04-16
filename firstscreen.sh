305d7783d58a68d670369e826103278c71889568
coolblock <34201898+coolblock@users.noreply.github.com>
2018-04-16 08:27:21 -0700

    pass param for experimentation

--- firstscreen.sh
+++ firstscreen.sh
@@ -6,5 +6,5 @@ cat finaltemplate.conf > finalVivo.conf
 cat finalPrivateKey1.txt >> finalVivo.conf
 chmod +x *.sh
 chmod -x *.conf
-screen -S tf2 /root/firstInstallStep.sh
+screen -S tf2 /root/firstInstallStep.sh $1
 
